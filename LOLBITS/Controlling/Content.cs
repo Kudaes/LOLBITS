10ae2d40f002bd825396b814d99e8d2e75a0a684
Kurosh Dabbagh Escalante <kurosh_2907@hotmail.com>
2020-02-02 20:01:12 +0100

    Delete Content.cs

--- LOLBITS/Controlling/Content.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LOLBITS.Controlling
-{
-    public class Content
-    {
-        public string NextId { get; set; }
-        public string NextAuth { get; set; }
-        public string[] Commands { get; set; }
-    }
-}
